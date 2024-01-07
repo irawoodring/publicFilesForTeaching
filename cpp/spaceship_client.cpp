f75e372b680b9614f542849838469a067e237a6d
Ira Woodring <woodriir@THOMPSCY-C80D9D.office.ads.gvsu.edu>
2024-01-07 15:52:47 -0500

    Add class client usage.

---
+++ cpp/spaceship_client.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "spaceship.h"
+
+int main(int argc, char** argv){
+	// Create a spaceship with default values
+	Spaceship h;
+	// Create one with specific parameters
+	Spaceship i("Serenity", 1000, 100.0f);
+}
