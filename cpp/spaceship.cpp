1e214bda6def985a7f58166b9c26fc190e8b940f
Ira Woodring <woodriir@THOMPSCY-C80D9D.office.ads.gvsu.edu>
2024-01-07 15:44:38 -0500

    Add muli-file example of classes in C++.

---
+++ cpp/spaceship.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include "spaceship.h"
+
+Spaceship::Spaceship(){
+  name = "No name";
+  hp = 500;
+  shield = 100.0f
+}
+
+Spaceship::Spaceship(std::string name, int hp, float shield){
+  this->name = name;
+  this->hp = hp;
+  this->shield = shield;
+}
